9a91052690a34de2dc36194f390e0f4a8d4fae4a
Sarah Gillie <sgillie@ttu.edu>
2025-04-10 21:52:28 -0500

    Update Bestseller.jsx

--- src/components/Bestseller.jsx
+++ src/components/Bestseller.jsx
@@ -11,7 +11,6 @@ const Bestseller = () => {
     useEffect(()=> {
         const bestProduct = products.filter((item)=>(item.bestseller));
         setBestSeller(bestProduct.slice(0,8))
-
     }, [])
 
   return (
